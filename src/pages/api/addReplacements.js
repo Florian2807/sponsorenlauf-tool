ff3bc2cfe01fc434454adb8d420ab4276add4ba2
Flo <84639717+Florian2807@users.noreply.github.com>
2024-10-18 22:46:24 +0000

    feat: add replacement User to db

--- src/pages/api/addReplacements.js
+++ src/pages/api/addReplacements.js
@@ -8,64 +8,60 @@ export default async function handler(req, res) {
 
 	const { amount } = req.body;
 
-	if (err) {
-		console.error('Fehler beim Öffnen der Datenbank:', err.message);
-		res.status(500).json({ message: 'Database connection error' });
-		return;
-	}
 
 	const getMaxIdPromise = () => {
-	return new Promise((resolve, reject) => {
-		db.get(`SELECT MAX(id) as maxId FROM students`, [], (err, row) => {
-			if (err) {
-				reject(err);
-			} else {
-				resolve(row.maxId || 0);
-			}
-		});
-	});
-
-
-const maxId = await getMaxIdPromise();
-let insertedCount = 0;
-
-// Daten in die Datenbank einfügen
-db.serialize(async () => {
-	const insertQuery = `INSERT INTO students (id, vorname, nachname, klasse, timestamps) VALUES (?, ?, ?, ?, ?)`;
-
-	const getMaxReplacementID = async () => {
 		return new Promise((resolve, reject) => {
-			db.get(
-				`SELECT MAX(CAST(vorname AS INT)) AS maxReplacementID FROM students WHERE klasse="Ersatz"`,
-				[],
-				(err, row) => {
+			db.get(`SELECT MAX(id) as maxId FROM students`, [], (err, row) => {
+				if (err) {
+					reject(err);
+				} else {
+					resolve(row.maxId || 0);
+				}
+			});
+		});
+	}
+
+	const maxId = await getMaxIdPromise();
+	let insertedCount = 0;
+
+	// Daten in die Datenbank einfügen
+	db.serialize(async () => {
+		const insertQuery = `INSERT INTO students (id, vorname, nachname, klasse, timestamps) VALUES (?, ?, ?, ?, ?)`;
+
+		const getMaxReplacementID = async () => {
+			return new Promise((resolve, reject) => {
+				db.get(
+					`SELECT MAX(CAST(vorname AS INT)) AS maxRID FROM students WHERE klasse="Ersatz"`,
+					[],
+					(err, row) => {
+						if (err) {
+							reject(err);
+						} else {
+							resolve(row.maxRID || 0);
+						}
+					},
+				);
+			});
+		};
+
+		const maxRID = await getMaxReplacementID();
+
+		for (let i = 0; i < amount; i++) {
+			const newId = parseInt(maxId + i + 1);
+			const newRID = maxRID + i + 1;
+			db.run(
+				insertQuery,
+				[newId, newRID, name, 'Ersatz', '[]'],
+				(err) => {
 					if (err) {
-						reject(err);
+						console.error('Fehler beim Einfügen:', err.message);
 					} else {
-						resolve(row.maxReplacementID || 0);
+						console.log(`Datensatz ${newId} erfolgreich eingefügt.`);
+						insertedCount++;
 					}
 				},
 			);
-		});
-	};
-
-	const maxReplacementID = await getMaxReplacementID();
-	console.log(maxReplacementID);
-
-	for (let i = 0; i < maxReplacementID + amount; i++) {
-		const newId = parseInt(maxId + i + 1);
-		const newReplacementID = maxReplacementID + i;
-		db.run(
-			insertQuery,
-			[newId, newReplacementID, '', 'Ersatz', '[]'],
-			(err) => {
-				if (err) {
-					console.error('Fehler beim Einfügen:', err.message);
-				} else {
-					console.log(`Datensatz ${newId} erfolgreich eingefügt.`);
-					insertedCount++;
-				}
-			},
-		);
-	}
-});
+		}
+	});
+	res.status(200).json({ count: insertedCount });
+}
